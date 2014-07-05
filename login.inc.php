ef170f4b532c84df248dcdb25543506803315ffb
renzotev <renzotelenta@gmail.com>
2014-07-04 22:42:54 -0500

    cambios

--- login.inc.php
+++ login.inc.php
@@ -48,93 +48,25 @@ Warehouse('header');
 	echo "<BODY onLoad='document.loginform.USERNAME.focus();  delete_cookie(\"dhtmlgoodies_tab_menu_tabIndex\");'>";
 	echo "";
 	
-	echo "
-	<form name=loginform method='post' action='index.php'>
-	<table width='100%' height='100%' border='0' cellspacing='0' cellpadding='0'>
-  <tr>
-    <td valign='middle' height='100%'><table class='wrapper' border='0' cellspacing='0' cellpadding='0' align='center'>
-        
-        <tr>
-          <td class='header'><table width='100%' border='0' cellspacing='0' cellpadding='0' class='logo_padding'>
-              <tr>
-                <td><img src='assets/osis_logo.png' border='0' /></td>
-                <td align='right'><a href='http://www.os4ed.com' target=_blank ><img src='assets/os4ed_logo.png' height='62' width='66' border='0'/></a></td>
-              </tr>
-            </table></td>
-        </tr>
-        <tr>
-          <td class='content'><table width='100%' border='0' cellspacing='0' cellpadding='0'>
-              <tr>
-                <td><table width='100%' border='0' cellspacing='0' cellpadding='0'>
-                    <tr>
-                      <td class='header_padding'><table width='100%' border='0' cellspacing='0' cellpadding='0'>
-                          <tr>
-                            <td class='header_txt'>Student Information System</td>
-                          </tr>
-                        </table></td>
-                    </tr>";
-					if($_REQUEST['maintain']=='Y'){
-				echo "<tr><td align='center' style='color:red'><b>"."openSIS is under maintenance and login privileges have been turned off. Please log in when it is available again."."</b></td></tr>";
-					}
-                    echo "<tr>
-                      <td class='padding'><table width='100%' border='0' cellspacing='0' cellpadding='0'>
-                          <tr>
-                            <td>
-                
-				<table border='0' width='100%' cellspacing='2' cellpadding='2' align=center>
-                 
-                  <tr>
-                    <td width='40%' align='right'>User Name :</td>
-                    <td width='60%'><input name='USERNAME' type='text' class='login_txt'></td>
-                  </tr>
-                  <tr>
-                    <td align='right'>Password :</td>
-                    <td><input name='PASSWORD' class='login_txt' type='password' AUTOCOMPLETE = 'off'></td>
-                  </tr>
-				  <tr><td colspan=2>
-				  " ;
-				  
-				  if($_REQUEST['reason'])
-				$note[] = 'You must have javascript enabled to use openSIS.';
-				echo ErrorMessage($error,'Error');
-	
-				  echo "
-				  </td></tr><tr>
-                    <td></td>
-                    <td><input name='' type='submit' class='login' value='' onMouseDown=Set_Cookie('dhtmlgoodies_tab_menu_tabIndex','',-1) />
-                    </td>
-                  </tr>
-				  </table>
-				  </td>
-                          </tr>
-                          <tr>
-                            <td align='center'><p style='padding:6px;'>".$log_msg['MESSAGE']."</p></td>
-                          </tr>
-                        </table></td>
-                    </tr>
-                  </table>
-              </tr>
-            </table>
-        <tr>
-          <td class='footer' valign='top'><table width='100%' border='0' cellspacing='0' cellpadding='0'>
-              <tr>
-                <td class='margin'></td>
-              </tr>
-              <tr>
-                <td align='center' class='copyright'>
-                openSIS is a product of Open Solutions for Education, Inc. (<a href='http://www.os4ed.com' target='_blank'>OS4Ed</a>).
-                and is licensed under the <a href='http://www.gnu.org/licenses/gpl.html' target='_blank'>GPL License</a>.
-                </td>
-              </tr>
-            </table></td>
-        </tr>
-      </table></td>
-  </tr>
-</table>
-</td>
-</tr>
-</table></form>
-";
+	echo "<div class='container' style='max-width:330px'>
+	<form name=loginform method='post' class='form-signin' action='index.php' role='form'>
+    <h2 class='form-signin-heading'><img src='assets/logoupc.png' width='100%' ></h2>
+    <input name='USERNAME' placeholder='User' type='text' class='form-control' required autofocus>
+
+    <input name='PASSWORD' class='form-control' placeholder='Password' type='password' AUTOCOMPLETE = 'off' required>
+    <input name='' type='submit' class='btn btn-lg btn-primary btn-block' value='Send' onMouseDown=Set_Cookie('dhtmlgoodies_tab_menu_tabIndex','',-1) />";
+    echo "</form>";
+    if($_REQUEST['maintain']=='Y'){
+        echo "<div class='alert alert-danger' role='alert'>openSIS is under maintenance and login privileges have been turned off. Please log in when it is available again.</div>";
+          }
+      $mesanjeError = ErrorMessage($error,'Error');
+      if ($mesanjeError != null) {
+        echo "<div class='alert alert-danger' role='alert'>".$mesanjeError."</div>";
+      }
+     
+  echo "<div class='alert alert-info' role='alert'>".$log_msg['MESSAGE']."</div>";
+  echo '</div>';
+    
 
 	Warehouse("footer");
 ?>
